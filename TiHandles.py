5443c9560d9e45cb9869b4bbeebf6c575405af34
smoothie-ws <kibot.email@gmail.com>
2023-12-14 23:35:25 +1000

    Package structure reorganized

--- TiHandles.py
+++ TiHandles.py
@@ -1,45 +1,56 @@
 from ctypes import Structure, POINTER
 
+__all__ = ['TiRuntime',
+           'TiAotModule',
+           'TiMemory',
+           'TiImage',
+           'TiKernel',
+           'TiComputeGraph',
+           'TiSampler']
+
 
 class _TiRuntime_t(Structure):
-    """Description placeholder"""
+    """Taichi runtime represents an instance of a logical backend and its internal dynamic state.
+    The user is responsible to synchronize any use of TiRuntime.
+    The user must not manipulate multiple TiRuntimes in the same thread."""
     pass
 
 
 class _TiAotModule_t(Structure):
-    """Description placeholder"""
+    """An ahead-of-time (AOT) compiled Taichi module, which contains a collection of kernels and compute graphs."""
     pass
 
 
 class _TiMemory_t(Structure):
-    """Description placeholder"""
-    pass
-
-
-class _TiSampler_t(Structure):
-    """Description placeholder"""
+    """A contiguous allocation of device memory."""
     pass
 
 
 class _TiImage_t(Structure):
-    """Description placeholder"""
+    """A contiguous allocation of device image."""
     pass
 
 
 class _TiKernel_t(Structure):
-    """Description placeholder"""
+    """A Taichi kernel that can be launched on the offload target for execution."""
     pass
 
 
 class _TiComputeGraph_t(Structure):
-    """Description placeholder"""
+    """A collection of Taichi kernels (a compute graph) to launch on the offload target in a predefined order."""
+    pass
+
+
+class _TiSampler_t(Structure):
+    """An image sampler. Represents a default image sampler provided by the runtime implementation.
+     The filter modes and address modes of default samplers depend on backend implementation."""
     pass
 
 
 TiRuntime = POINTER(_TiRuntime_t)
 TiAotModule = POINTER(_TiAotModule_t)
 TiMemory = POINTER(_TiMemory_t)
-TiSampler = POINTER(_TiSampler_t)
 TiImage = POINTER(_TiImage_t)
 TiKernel = POINTER(_TiKernel_t)
 TiComputeGraph = POINTER(_TiComputeGraph_t)
+TiSampler = POINTER(_TiSampler_t)
--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from ctypes import byref, create_string_buffer, string_at, cast
 from typing import List
 
 from .TiAliases import *
@@ -7,100 +6,110 @@ from .TiEnumerations import *
 from .TiFunctions import *
 from .TiHandles import *
 from .TiStructures import *
+from .TiWrapper import *
+
+__all__ = ['TiAliases',
+           'TiDefinitions',
+           'TiEnumerations',
+           'TiFunctions',
+           'TiHandles',
+           'TiStructures',
+           'get_version',
+           'get_last_error',
+           'set_last_error',
+           'create_runtime',
+           'allocate_memory',
+           'free_memory',
+           'map_memory',
+           'unmap_memory',
+           'get_available_archs',
+           'destroy_runtime',
+           'allocate_image',
+           'free_image',
+           'create_sampler',
+           'destroy_sampler',
+           'copy_memory_device_to_device',
+           'copy_image_device_to_device',
+           'track_image_ext',
+           'transition_image',
+           'launch_kernel',
+           'launch_compute_graph',
+           'flush',
+           'wait',
+           'load_aot_module',
+           'create_aot_module',
+           'destroy_aot_module',
+           'get_aot_module_kernel',
+           'get_aot_module_compute_graph']
 
 
 # Error Handling
 def get_version() -> c_uint32:
-    return ti_get_version()
+    return _py_ti_get_version()
 
 
-def get_last_error() -> str:
-    message_size = c_uint64(256)
-    message_buffer = create_string_buffer(message_size.value)
-
-    error_code = ti_get_last_error(byref(message_size), message_buffer)
-    error_message = string_at(message_buffer, message_size.value).decode('utf-8')
+def get_last_error(message_size: int) -> str:
+    error_message, error_code = _py_ti_get_last_error(message_size)
 
     return f"Taichi C-API returned: {error_message}. Error code: {error_code}"
 
 
 def set_last_error(error: TiError, message: str) -> None:
-    ti_set_last_error(error, message.encode('utf-8'))
-
-
-def create_runtime(arch: TiArch,
-                   device: int) -> TiRuntime:
-    """Description placeholder"""
-
-    address = ti_create_runtime(arch, device)
-    return cast(address, TiRuntime)
+    _py_ti_set_last_error(error, message)
 
 
 # Memory Operations
 def allocate_memory(runtime: TiRuntime, allocate_info: TiMemoryAllocateInfo) -> TiMemory:
-    address = ti_allocate_memory(runtime, byref(allocate_info))
-    return cast(address, TiMemory)
+    return _py_ti_allocate_memory(runtime, allocate_info)
 
 
 def free_memory(runtime: TiRuntime, memory: TiMemory) -> None:
-    ti_free_memory(runtime, memory)
+    _py_ti_free_memory(runtime, memory)
 
 
 def map_memory(runtime: TiRuntime, memory: TiMemory) -> c_void_p:
-    return ti_map_memory(runtime, memory)
+    return _py_ti_map_memory(runtime, memory)
 
 
 def unmap_memory(runtime: TiRuntime, memory: TiMemory) -> None:
-    ti_unmap_memory(runtime, memory)
+    _py_ti_unmap_memory(runtime, memory)
 
 
 def get_available_archs() -> list[str]:
     """Gets a list of available archs on the current platform."""
 
-    arch_count = c_uint32(TI_MAX_ARCH_COUNT)
-    arch_list = (c_uint32 * TI_MAX_ARCH_COUNT)()
-
-    ti_get_available_archs(byref(arch_count), arch_list)
-
+    arch_count, arch_list = _py_ti_get_available_archs()
     arch_values = [TiArch(arch_list[i]) for i in range(arch_count.value)]
-    arch_names = [arch.name for arch in arch_values]
 
-    return arch_names
-
-
-def destroy_runtime(runtime: TiRuntime) -> None:
-    """Description placeholder"""
-
-    ti_destroy_runtime(runtime)
+    return [arch.name for arch in arch_values]
 
 
 def allocate_image(runtime: TiRuntime,
                    allocate_info: TiImageAllocateInfo) -> c_void_p:
     """Description placeholder"""
 
-    return ti_allocate_image(runtime, byref(allocate_info))
+    return _py_ti_allocate_image(runtime, allocate_info)
 
 
 def free_image(runtime: TiRuntime,
                image: TiImage) -> None:
     """Description placeholder"""
 
-    ti_free_image(runtime, image)
+    _py_ti_free_image(runtime, image)
 
 
 def create_sampler(runtime: TiRuntime,
                    create_info: TiSamplerCreateInfo) -> TiSampler:
     """Description placeholder"""
 
-    address = ti_create_sampler(runtime, byref(create_info))
-    return cast(address, TiSampler)
+    return _py_ti_create_sampler(runtime, create_info)
 
 
 def destroy_sampler(runtime: TiRuntime,
                     sampler: TiSampler) -> None:
     """Description placeholder"""
 
-    ti_destroy_sampler(runtime, sampler)
+    _py_ti_destroy_sampler(runtime, sampler)
 
 
 def copy_memory_device_to_device(runtime: TiRuntime,
@@ -108,7 +117,7 @@ def copy_memory_device_to_device(runtime: TiRuntime,
                                  dst: TiMemorySlice) -> None:
     """Description placeholder"""
 
-    ti_copy_memory_device_to_device(runtime, byref(src), byref(dst))
+    _py_ti_copy_memory_device_to_device(runtime, src, dst)
 
 
 def copy_image_device_to_device(runtime: TiRuntime,
@@ -116,7 +125,7 @@ def copy_image_device_to_device(runtime: TiRuntime,
                                 dst: TiImageSlice) -> None:
     """Description placeholder"""
 
-    ti_copy_image_device_to_device(runtime, byref(src), byref(dst))
+    _py_ti_copy_image_device_to_device(runtime, src, dst)
 
 
 def track_image_ext(runtime: TiRuntime,
@@ -124,7 +133,7 @@ def track_image_ext(runtime: TiRuntime,
                     layout: TiImageLayout) -> None:
     """Description placeholder"""
 
-    ti_track_image_ext(runtime, image, layout)
+    _py_ti_track_image_ext(runtime, image, layout)
 
 
 def transition_image(runtime: TiRuntime,
@@ -132,47 +141,35 @@ def transition_image(runtime: TiRuntime,
                      layout: TiImageLayout) -> None:
     """Description placeholder"""
 
-    ti_transition_image(runtime, image, layout)
+    _py_ti_transition_image(runtime, image, layout)
 
 
 def launch_kernel(runtime: TiRuntime,
                   kernel: TiKernel,
-                  num_args: int,
                   args: List[TiArgument]) -> None:
-    """Launches a Taichi kernel with the provided arguments.
+    """Description placeholder"""
 
-    The arguments must have the same count and types in the same order as in the source code.
-
-    Parameters:
-    - runtime (Type: TiRuntime): The runtime on which the kernel will be launched.
-    - kernel (Type: TiKernel): The kernel to be launched.
-    - num_args (Type: int): Number of kernel arguments.
-    - args (Type: List[TiArgument]): List of kernel arguments.
-
-    Returns: None
-    """
-    ti_launch_kernel(runtime, kernel, num_args, byref((TiArgument * num_args)(*args)))
+    _py_ti_launch_kernel(runtime, kernel, len(args), args)
 
 
 def launch_compute_graph(runtime: TiRuntime,
                          graph: TiComputeGraph,
-                         num_args: int,
                          named_args: List[TiNamedArgument]) -> None:
     """Description placeholder"""
 
-    ti_launch_compute_graph(runtime, graph, num_args, (TiNamedArgument * num_args)(*named_args))
+    _py_ti_launch_compute_graph(runtime, graph, len(named_args), named_args)
 
 
 def flush(runtime: TiRuntime) -> None:
     """Description placeholder"""
 
-    ti_flush(runtime)
+    _py_ti_flush(runtime)
 
 
 def wait(runtime: TiRuntime) -> None:
     """Description placeholder"""
 
-    ti_wait(runtime)
+    _py_ti_wait(runtime)
 
 
 def load_aot_module(runtime: TiRuntime,
@@ -180,10 +177,22 @@ def load_aot_module(runtime: TiRuntime,
     """Description placeholder"""
 
     folder = dirname(abspath(__name__))
-    aot_module_path: bytes = join(folder, filepath).encode('utf-8')
+    aot_module_path = join(folder, filepath)
 
-    address = ti_load_aot_module(runtime, aot_module_path)
-    return cast(address, TiAotModule)
+    return _py_ti_load_aot_module(runtime, aot_module_path)
+
+
+def create_runtime(arch: TiArch,
+                   device: int) -> TiRuntime:
+    """Description placeholder"""
+
+    return _py_ti_create_runtime(arch, device)
+
+
+def destroy_runtime(runtime: TiRuntime) -> None:
+    """Description placeholder"""
+
+    _py_ti_destroy_runtime(runtime)
 
 
 def create_aot_module(runtime: TiRuntime,
@@ -191,26 +200,24 @@ def create_aot_module(runtime: TiRuntime,
                       size: int) -> c_void_p:
     """Description placeholder"""
 
-    return ti_create_aot_module(runtime, data, size)
+    return _py_ti_create_aot_module(runtime, data, size)
 
 
 def destroy_aot_module(module: TiAotModule) -> None:
     """Description placeholder"""
 
-    ti_destroy_aot_module(module)
+    _py_ti_destroy_aot_module(module)
 
 
 def get_aot_module_kernel(module: TiAotModule,
                           kernel_name: str) -> TiKernel:
     """Description placeholder"""
 
-    address = ti_get_aot_module_kernel(module, kernel_name.encode('utf-8'))
-    return cast(address, TiKernel)
+    return _py_ti_get_aot_module_kernel(module, kernel_name)
 
 
 def get_aot_module_compute_graph(module: TiAotModule,
                                  graph_name: str) -> TiComputeGraph:
     """Description placeholder"""
 
-    address = ti_get_aot_module_compute_graph(module, graph_name.encode('utf-8'))
-    return cast(address, TiComputeGraph)
+    return _py_ti_get_aot_module_compute_graph(module, graph_name)
