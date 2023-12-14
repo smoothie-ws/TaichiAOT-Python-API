6af5d81c93649050ea1c85be3a555f538b461671
smoothie-ws <kibot.email@gmail.com>
2023-12-14 23:28:36 +1000

    Package structure reorganized

--- TiStructures.py
+++ TiStructures.py
@@ -1,172 +1,185 @@
-from ctypes import Structure, c_uint32, c_uint64, c_bool, c_float, c_uint8, Union, c_uint16, c_int32, c_char_p, POINTER
+"""Module providing ctypes structures for TiRuntime."""
+
+from ctypes import Structure, c_uint32, c_uint64, c_float, c_uint8, Union, c_uint16, c_int32, c_char_p
 
 from .TiAliases import TiBool
+from .TiEnumerations import *
 from .TiHandles import TiMemory, TiImage, TiSampler
 
 
-class TiCapabilityLevelInfo(Structure):
-    """Description placeholder"""
+class CapabilityLevelInfo(Structure):
+    """An integral device capability level.
+    It currently is not guaranteed that a higher level value is compatible with a lower level value."""
 
     _fields_ = [
-        ("capability", c_uint32),
+        ("capability", TiCapability),
         ("level", c_uint32)
     ]
 
 
 class TiMemoryAllocateInfo(Structure):
     """Parameters of a newly allocated memory."""
-
     _fields_ = [
         ("size", c_uint64),  # Size of the allocation in bytes.
         ("host_write", TiBool),  # True if the host needs to write to the allocated memory.
         ("host_read", TiBool),  # True if the host needs to read from the allocated memory.
-        ("export_sharing", TiBool),  # True if the memory allocation needs to be exported to other backends
-        ("usage", c_uint32)  # All possible usage of this memory allocation.
+        ("export_sharing", TiBool),  # True if the memory allocation needs to be exported to other backends.
+        ("usage", TiMemoryUsageFlags)  # All possible usage of this memory allocation.
     ]
 
 
 class TiMemorySlice(Structure):
-    """Description placeholder"""
+    """A subsection of a memory allocation."""
 
     _fields_ = [
-        ("memory", TiMemory),
-        ("offset", c_uint64),
-        ("size", c_uint64)
+        ("memory", TiMemory),  # The subsectioned memory allocation.
+        ("offset", c_uint64),  # Offset from the beginning of the allocation.
+        ("size", c_uint64)  # Size of the subsection.
     ]
 
 
 class TiNdShape(Structure):
-    """Description placeholder"""
+    """Multi-dimensional size of an ND-array."""
 
     _fields_ = [
-        ("dim_count", c_uint32),
-        ("dims", c_uint32 * 16)
+        ("dim_count", c_uint32),  # Number of dimensions.
+        ("dims", c_uint32 * 16)  # Dimension sizes.
     ]
 
 
 class TiNdArray(Structure):
-    """Description placeholder"""
+    """Represents an N-dimensional array."""
 
     _fields_ = [
-        ("memory", TiMemory),
-        ("shape", TiNdShape),
-        ("elem_shape", TiNdShape),
-        ("elem_type", c_uint32)
+        ("memory", TiMemory),  # Memory bound to the ND-array.
+        ("shape", TiNdShape),  # Shape of the ND-array.
+        ("elem_shape", TiNdShape),  # Shape of the ND-array elements.
+        ("elem_type", TiDataType)  # Primitive data type of the ND-array elements.
     ]
 
 
 class TiImageOffset(Structure):
-    """Description placeholder"""
+    """Offsets of an image in X, Y, Z, and array layers."""
 
     _fields_ = [
-        ("array_layer_offset", c_uint32)
+        ("x", c_uint32),  # Image offset in the X direction.
+        ("y", c_uint32),  # Image offset in the Y direction. Must be 0 for 1D or 1D array.
+        ("z", c_uint32),  # Image offset in the Z direction. Must be 0 for 1D, 2D, or cube array.
+        ("array_layer_offset", c_uint32)  # Image offset in array layers. Must be 0 for 1D, 2D, or 3D array.
     ]
 
 
 class TiImageExtent(Structure):
-    """Description placeholder"""
+    """Extents of an image in X, Y, Z, and array layers."""
 
     _fields_ = [
-        ("width", c_uint32),
-        ("height", c_uint32),
-        ("depth", c_uint32),
+        ("width", c_uint32),  # Image extent in the X direction.
+        ("height", c_uint32),  # Image extent in the Y direction. Must be 1 for 1D or 1D array.
+        ("depth", c_uint32),  # Image extent in the Z direction. Must be 1 for 1D, 2D or cube array.
         ("array_layer_count", c_uint32)
+        # Image extent in array layers. Must be 1 for 1D, 2D, or 3D array. Must be 6 for cube array.
     ]
 
 
 class TiImageAllocateInfo(Structure):
-    """Description placeholder"""
+    """Parameters of a newly allocated image."""
 
     _fields_ = [
-        ("dimension", c_uint32),
-        ("extent", TiImageExtent),
-        ("mip_level_count", c_uint32),
-        ("format", c_uint32),
-        ("export_sharing", c_bool),
-        ("usage", c_uint32)
+        ("dimension", TiImageDimension),  # Image dimension.
+        ("extent", TiImageExtent),  # Image extent.
+        ("mip_level_count", c_uint32),  # Number of mip-levels.
+        ("format", TiFormat),  # Image texel format.
+        ("export_sharing", TiBool),  # True if the memory allocation needs to be exported to other backends.
+        ("usage", TiImageUsageFlags)  # All possible usages of this image allocation.
     ]
 
 
 class TiImageSlice(Structure):
-    """Description placeholder"""
+    """Represents a slice of an image."""
 
     _fields_ = [
-        ("image", TiImage),
-        ("offset", TiImageOffset),
-        ("extent", TiImageExtent),
-        ("mip_level", c_uint32)
+        ("image", TiImage),  # The subsectioned image allocation.
+        ("offset", TiImageOffset),  # Offset from the beginning of the allocation in each dimension.
+        ("extent", TiImageExtent),  # Size of the subsection in each dimension.
+        ("mip_level", c_uint32)  # The subsectioned mip-level.
     ]
 
 
 class TiSamplerCreateInfo(Structure):
-    """Description placeholder"""
+    """Parameters for creating a sampler."""
 
     _fields_ = [
-        ("mag_filter", c_uint32),
-        ("min_filter", c_uint32),
-        ("address_mode", c_uint32),
-        ("max_anisotropy", c_float)
+        ("mag_filter", TiFilter),  # Magnification filter.
+        ("min_filter", TiFilter),  # Minification filter.
+        ("address_mode", TiAddressMode),  # Address mode.
+        ("max_anisotropy", c_float)  # Maximum anisotropy level.
     ]
 
 
 class TiTexture(Structure):
-    """Description placeholder"""
+    """Image data bound to a sampler."""
 
     _fields_ = [
-        ("image", TiImage),
-        ("sampler", TiSampler),
-        ("dimension", c_uint32),
-        ("extent", TiImageExtent),
-        ("format", c_uint32)
+        ("image", TiImage),  # Image bound to the texture.
+        ("sampler", TiSampler),  # The bound sampler that controls the sampling behavior of `structure.texture.image`.
+        ("dimension", TiImageDimension),  # Image Dimension.
+        ("extent", TiImageExtent),  # Image extent.
+        ("format", TiFormat)  # Image texel format.
     ]
 
 
 class TiScalarValue(Union):
-    """Description placeholder"""
+    """Scalar value represented by a power-of-two number of bits."""
+    """NOTE:
+        The unsigned integer types merely hold the number of bits in memory
+        and doesn't reflect any type of the underlying data. For example, a 32-bit
+        floating-point scalar value is assigned by `*(float*)&scalar_value.x32 =
+        0.0f`; a 16-bit signed integer is assigned by `*(int16_t)&scalar_vaue.x16 =
+        1`. The actual type of the scalar is hinted via `type`."""
 
     _fields_ = [
-        ("x8", c_uint8),
-        ("x16", c_uint16),
-        ("x32", c_uint32),
-        ("x64", c_uint64)
+        ("x8", c_uint8),  # Scalar value that fits into 8 bits.
+        ("x16", c_uint16),  # Scalar value that fits into 16 bits.
+        ("x32", c_uint32),  # Scalar value that fits into 32 bits.
+        ("x64", c_uint64)  # Scalar value that fits into 64 bits.
     ]
 
 
 class TiScalar(Structure):
-    """Description placeholder"""
+    """A typed scalar value."""
 
     _fields_ = [
-        ("type", c_uint32),
-        ("value", TiScalarValue)
+        ("type", TiDataType),  # Data type of the scalar.
+        ("value", TiScalarValue)  # Value of the scalar.
     ]
 
 
 class TiTensorValue(Structure):
-    """Description placeholder"""
+    """Tensor value represented by a power-of-two number of bits."""
 
     _fields_ = [
-        ("x8", c_uint8 * 128),
-        ("x16", c_uint16 * 64),
-        ("x32", c_uint32 * 32),
-        ("x64", c_uint64 * 16)
+        ("x8", c_uint8 * 128),  # Tensor value that fits into 8 bits.
+        ("x16", c_uint16 * 64),  # Tensor value that fits into 16 bits.
+        ("x32", c_uint32 * 32),  # Tensor value that fits into 32 bits.
+        ("x64", c_uint64 * 16)  # Tensor value that fits into 64 bits.
     ]
 
 
 class TiTensorValueWithLength(Structure):
-    """Description placeholder"""
+    """A tensor value with a length."""
 
     _fields_ = [
-        ("length", c_uint32),
-        ("data", TiTensorValue)
+        ("length", c_uint32),  # Length of the tensor.
+        ("data", TiTensorValue)  # Data of the tensor.
     ]
 
 
 class TiTensor(Structure):
-    """Description placeholder"""
+    """A typed tensor value."""
 
     _fields_ = [
-        ("type", c_uint32),
-        ("contents", TiTensorValueWithLength)
+        ("type", TiDataType),  # Data type of the tensor.
+        ("contents", TiTensorValueWithLength)  # Contents of the tensor.
     ]
 
 
@@ -174,27 +187,28 @@ class TiArgumentValue(Union):
     """A scalar or structured argument value."""
 
     _fields_ = [
-        ("i32", c_int32),
-        ("f32", c_float),
-        ("ndarray", TiNdArray),
-        ("texture", TiTexture),
-        ("scalar", TiScalar),
-        ("tensor", TiTensor)
+        ("i32", c_int32),  # Value of a 32-bit one's complement signed integer.
+        ("f32", c_float),  # Value of a 32-bit IEEE 754 single-precision floating-poing number.
+        ("ndarray", TiNdArray),  # An ND-array to be bound.
+        ("texture", TiTexture),  # A texture to be bound.
+        ("scalar", TiScalar),  # A scalar to be bound.
+        ("tensor", TiTensor)  # A tensor to be bound.
     ]
 
 
 class TiArgument(Structure):
-    """Description placeholder"""
+    """An argument value to feed kernels."""
 
-    _fields_ = [("type", c_uint32),
-                ("value", TiArgumentValue)
-                ]
+    _fields_ = [
+        ("type", TiArgumentType),  # Type of the argument.
+        ("value", TiArgumentValue)  # A Value of the argument.
+    ]
 
 
 class TiNamedArgument(Structure):
-    """Description placeholder"""
+    """A named argument value to feed compute graphs."""
 
     _fields_ = [
-        ("name", c_char_p),
-        ("argument", TiArgument)
+        ("name", c_char_p),  # Name of the argument.
+        ("argument", TiArgument)  # Argument body.
     ]
---
+++ TiWrapper.py
@@ -0,0 +1,231 @@
+"""Module providing Python bindings for TiRuntime functions."""
+
+from ctypes import byref, create_string_buffer, string_at, cast, Array, c_void_p
+from typing import List, Any
+
+from .TiDefinitions import *
+from .TiFunctions import *
+from .TiHandles import *
+from .TiStructures import *
+
+__all__ = ['_py_ti_get_version',
+           '_py_ti_get_last_error',
+           '_py_ti_set_last_error',
+           '_py_ti_create_runtime',
+           '_py_ti_allocate_memory',
+           '_py_ti_free_memory',
+           '_py_ti_map_memory',
+           '_py_ti_unmap_memory',
+           '_py_ti_get_available_archs',
+           '_py_ti_destroy_runtime',
+           '_py_ti_allocate_image',
+           '_py_ti_free_image',
+           '_py_ti_create_sampler',
+           '_py_ti_destroy_sampler',
+           '_py_ti_copy_memory_device_to_device',
+           '_py_ti_copy_image_device_to_device',
+           '_py_ti_track_image_ext',
+           '_py_ti_transition_image',
+           '_py_ti_launch_kernel',
+           '_py_ti_launch_compute_graph',
+           '_py_ti_flush',
+           '_py_ti_wait',
+           '_py_ti_load_aot_module',
+           '_py_ti_create_aot_module',
+           '_py_ti_destroy_aot_module',
+           '_py_ti_get_aot_module_kernel',
+           '_py_ti_get_aot_module_compute_graph']
+
+
+# Error Handling
+def _py_ti_get_version() -> c_uint32:
+    """Get the current taichi version."""
+
+    return ti_get_version()
+
+
+def _py_ti_get_last_error(size: int) -> tuple[str, int]:
+    """Gets the last error raised by Taichi C-API invocations.
+     Returns the semantical error message and error code."""
+
+    message_size = c_uint64(size)  # Size of textual error message in `function.get_last_error.message`
+
+    # Ignored when message_size is 0.
+    message_buffer = create_string_buffer(message_size.value)  # Text buffer for the textual error message.
+
+    error_code = ti_get_last_error(byref(message_size), message_buffer)
+    error_message = string_at(message_buffer, message_size.value).decode('utf-8')
+
+    return error_message, error_code
+
+
+def _py_ti_set_last_error(error: TiError, message: str) -> None:
+    """Sets the provided error as the last error raised by Taichi C-API invocations."""
+
+    ti_set_last_error(error, message.encode('utf-8'))
+
+
+# Memory Operations
+def _py_ti_allocate_memory(runtime: TiRuntime, allocate_info: TiMemoryAllocateInfo) -> TiMemory:
+    """Allocate memory on the TiRuntime."""
+
+    address = ti_allocate_memory(runtime, byref(allocate_info))
+    return cast(address, TiMemory)
+
+
+def _py_ti_free_memory(runtime: TiRuntime, memory: TiMemory) -> None:
+    """Free allocated memory on the TiRuntime."""
+
+    ti_free_memory(runtime, memory)
+
+
+def _py_ti_map_memory(runtime: TiRuntime, memory: TiMemory) -> c_void_p:
+    """Map allocated memory on the TiRuntime."""
+
+    return ti_map_memory(runtime, memory)
+
+
+def _py_ti_unmap_memory(runtime: TiRuntime, memory: TiMemory) -> None:
+    """Unmap allocated memory on the TiRuntime."""
+
+    ti_unmap_memory(runtime, memory)
+
+
+def _py_ti_get_available_archs() -> tuple[Any, Array[Any]]:
+    """Get available architectures on the TiRuntime."""
+
+    arch_count = c_uint32(TI_MAX_ARCH_COUNT)
+    arch_list = (c_uint32 * TI_MAX_ARCH_COUNT)()
+
+    ti_get_available_archs(byref(arch_count), arch_list)
+
+    return arch_count, arch_list
+
+
+def _py_ti_allocate_image(runtime: TiRuntime, allocate_info: TiImageAllocateInfo) -> TiImage:
+    """Allocate an image on the TiRuntime."""
+
+    return ti_allocate_image(runtime, byref(allocate_info))
+
+
+def _py_ti_free_image(runtime: TiRuntime, image: TiImage) -> None:
+    """Free an allocated image on the TiRuntime."""
+
+    ti_free_image(runtime, image)
+
+
+def _py_ti_create_sampler(runtime: TiRuntime, create_info: TiSamplerCreateInfo) -> TiSampler:
+    """Create a sampler on the TiRuntime."""
+
+    address = ti_create_sampler(runtime, byref(create_info))
+    return cast(address, TiSampler)
+
+
+def _py_ti_destroy_sampler(runtime: TiRuntime, sampler: TiSampler) -> None:
+    """Destroy a sampler on the TiRuntime."""
+
+    ti_destroy_sampler(runtime, sampler)
+
+
+def _py_ti_copy_memory_device_to_device(runtime: TiRuntime, src: TiMemorySlice, dst: TiMemorySlice) -> None:
+    """Copy memory from one device to another on the TiRuntime."""
+
+    ti_copy_memory_device_to_device(runtime, byref(src), byref(dst))
+
+
+def _py_ti_copy_image_device_to_device(runtime: TiRuntime, src: TiImageSlice, dst: TiImageSlice) -> None:
+    """Copy an image from one device to another on the TiRuntime."""
+
+    ti_copy_image_device_to_device(runtime, byref(src), byref(dst))
+
+
+def _py_ti_track_image_ext(runtime: TiRuntime, image: TiImage, layout: TiImageLayout) -> None:
+    """Track an image on the TiRuntime with an extended layout."""
+
+    ti_track_image_ext(runtime, image, layout)
+
+
+def _py_ti_transition_image(runtime: TiRuntime, image: TiImage, layout: TiImageLayout) -> None:
+    """Transition an image on the TiRuntime to a new layout."""
+
+    ti_transition_image(runtime, image, layout)
+
+
+def _py_ti_launch_kernel(runtime: TiRuntime, kernel: TiKernel, num_args: int, args: List[TiArgument]) -> None:
+    """Launches a Taichi kernel with the provided arguments.
+    The arguments must have the same count and types in the same order as in the source code.
+
+    Parameters:
+    - runtime (Type: TiRuntime): The runtime on which the kernel will be launched.
+    - kernel (Type: TiKernel): The kernel to be launched.
+    - num_args (Type: int): Number of kernel arguments.
+    - args (Type: List[TiArgument]): List of kernel arguments.
+
+    Returns: None"""
+
+    ti_launch_kernel(runtime, kernel, num_args, byref((TiArgument * num_args)(*args)))
+
+
+def _py_ti_launch_compute_graph(runtime: TiRuntime, graph: TiComputeGraph,
+                                num_args: int, named_args: List[TiNamedArgument]) -> None:
+    """Launch a compute graph on the TiRuntime."""
+
+    ti_launch_compute_graph(runtime, graph, num_args, (TiNamedArgument * num_args)(*named_args))
+
+
+def _py_ti_flush(runtime: TiRuntime) -> None:
+    """Flush operations on the TiRuntime."""
+
+    ti_flush(runtime)
+
+
+def _py_ti_wait(runtime: TiRuntime) -> None:
+    """Wait for operations on the TiRuntime to complete."""
+
+    ti_wait(runtime)
+
+
+def _py_ti_create_runtime(arch: TiArch, device: int) -> TiRuntime:
+    """Creates a Taichi Runtime with the specified TiArch."""
+
+    address = ti_create_runtime(arch, device)
+    return cast(address, TiRuntime)
+
+
+def _py_ti_destroy_runtime(runtime: TiRuntime) -> None:
+    """Destroys a Taichi Runtime."""
+
+    ti_destroy_runtime(runtime)
+
+
+def _py_ti_load_aot_module(runtime: TiRuntime, filepath: str) -> TiAotModule:
+    """Load an AOT module from a file on the TiRuntime."""
+
+    address = ti_load_aot_module(runtime, filepath.encode('utf-8'))
+    return cast(address, TiAotModule)
+
+
+def _py_ti_create_aot_module(runtime: TiRuntime, data: c_void_p, size: int) -> c_void_p:
+    """Create an AOT module on the TiRuntime from data."""
+
+    return ti_create_aot_module(runtime, data, size)
+
+
+def _py_ti_destroy_aot_module(module: TiAotModule) -> None:
+    """Destroy an AOT module on the TiRuntime."""
+
+    ti_destroy_aot_module(module)
+
+
+def _py_ti_get_aot_module_kernel(module: TiAotModule, kernel_name: str) -> TiKernel:
+    """Get a kernel from an AOT module on the TiRuntime."""
+
+    address = ti_get_aot_module_kernel(module, kernel_name.encode('utf-8'))
+    return cast(address, TiKernel)
+
+
+def _py_ti_get_aot_module_compute_graph(module: TiAotModule, graph_name: str) -> TiComputeGraph:
+    """Get a compute graph from an AOT module on the TiRuntime."""
+
+    address = ti_get_aot_module_compute_graph(module, graph_name.encode('utf-8'))
+    return cast(address, TiComputeGraph)
